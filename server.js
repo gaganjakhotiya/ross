f7b06d5ecf04e78d9e758b3cd1397920ccf492c6
Gagan Jakhotiya <gaganjakhotiya@outlook.com>
2024-07-14 22:01:26 +0530

    api prefix

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ app.get("/", (req, res) => {
   res.send("Hello from Ross!");
 });
 
-app.get("/slack/ross/command", slackRoss.command);
+app.get("/api/slack/ross/command", slackRoss.command);
+app.post("/api/slack/ross/command", slackRoss.command);
 
 app.listen(port, () => {
   console.log(`Ross server is running on http://localhost:${port}`);
